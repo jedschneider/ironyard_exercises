fe626737ab754bdc3d18bf8b212d995db688a3f3
Belinda Hare <hare.belinda@gmail.com>
2014-10-21 09:27:03 -0400

    Update belinda_anagram.rb

--- anagram_ruby/belinda_anagram.rb
+++ anagram_ruby/belinda_anagram.rb
@@ -3,16 +3,16 @@ class Anagram
     @word = gram
   end
   def match (match_word)
-    lukies_array=[]
+    word_array=[]
     match_word.each do |popcorn|
       if @word.downcase == popcorn.downcase
       #checking to see if word 1 matches word 2
       elsif @word.downcase.chars.sort == popcorn.downcase.chars.sort
-      lukies_array.push(popcorn)
+      word_array.push(popcorn)
       #breaking the word into character and adding word 2 to the blank array if it matches the characters in word 1
       end
     end
-    lukies_array
+    word_array
     #display array with word 2 matches 
   end
 end
