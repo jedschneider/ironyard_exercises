d91e6c7b5577d19b5de5000d6265a2694e0c8d22
Malachi Gray <graymd.cfi@gmail.com>
2015-03-08 13:30:36 -0400

    imperative anagram

--- anagram_ruby/mgray_anagram.rb
+++ anagram_ruby/mgray_anagram.rb
@@ -3,53 +3,91 @@
 # Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
 
 #Imperative:
-word = 'listen'
-array_word = []
-array_word.push(word.split(''))
-array_current_word = []
-candidates = ['enslists', 'google', 'inlets', 'banana', 'listen']
-anagrams = []
 
-x = 0
-y = 0
-while x < candidates.length do
+class Anagram
 
-#puts candidates[x]
-current_word = candidates[x].downcase
-if current_word != word
-# puts candidates[x]
-  if current_word.length == word.length
-    if current_word.chars.sort == word.chars.sort
-      anagrams.push(current_word)
+    def downcase_word(word)
+      @original_word = word.downcase
     end
-  end
-end
-x +=1
-end
-p anagrams
-
-
-
-anagrams_2 = []
-#declarative
-candidates.each do |candidate|
-  if candidate != word
-    if candidate.chars.sort == word.chars.sort
-      anagrams_2.push(candidate)
+    
+    def test(array)
+      anagram_array = []
+      x = 0
+      while x < array.length do
+        current_word = array[x]
+        if current_word.length == @original_word.length
+          if current_word.chars.sort == @original_word.chars.sort
+            anagram_array << current_word
+          end
+        end
+        x +=1
+      end
+      anagram_array 
     end
-  end
+
+
 end
-p anagrams_2
 
 
 
 
-# make both words into array.
-# check if all letters are in array of other
-# if yes good. no not!
 
 
-#try .include? 
+
+
+
+
+
+
+
+
+# word = 'listen'
+# array_word = []
+# array_word.push(word.split(''))
+# array_current_word = []
+# candidates = ['enslists', 'google', 'inlets', 'banana', 'listen']
+# anagrams = []
+
+# x = 0
+# y = 0
+# while x < candidates.length do
+
+# #puts candidates[x]
+# current_word = candidates[x].downcase
+# if current_word != word
+# # puts candidates[x]
+#   if current_word.length == word.length
+#     if current_word.chars.sort == word.chars.sort
+#       anagrams.push(current_word)
+#     end
+#   end
+# end
+# x +=1
+# end
+# p anagrams
+
+
+
+# anagrams_2 = []
+# #declarative
+# candidates.each do |candidate|
+#   if candidate != word
+#     if candidate.chars.sort == word.chars.sort
+#       anagrams_2.push(candidate)
+#     end
+#   end
+# end
+# p anagrams_2
+
+
+
+
+# # make both words into array.
+# # check if all letters are in array of other
+# # if yes good. no not!
+
+
+# #try .include? 
 
 
 
