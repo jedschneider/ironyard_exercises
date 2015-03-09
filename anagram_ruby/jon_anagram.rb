b924f40fe2b327f2d77d7377dd92ae0a9ddf7db8
Jon <jondotcourtney@gmail.com>
2015-03-09 13:50:11 -0400

    added imperative ruby

--- anagram_ruby/jon_anagram.rb
+++ anagram_ruby/jon_anagram.rb
@@ -1,4 +1,18 @@
+# Imperative
+
 class Anagram
   def initialize(word)
-    @word = word
+    @the_word = word
   end
+
+  def match(match)
+    @results = []
+    match.each do |wordmatch|
+      if @the_word.downcase == wordmatch.downcase
+      elsif @the_word.downcase.chars.sort == wordmatch.downcase.chars.sort
+        @results << wordmatch
+      end
+    end
+    @results
+  end
+end
