5760381b007a2a9d2ce7bd64f6c26d799e90725a
Adam Kelsey <adam.kelsey113@gmail.com>
2015-03-09 12:00:35 -0400

    fixed code for test

--- anagram_ruby/anagram.rb
+++ anagram_ruby/anagram.rb
@@ -3,29 +3,28 @@
 class Anagram
 
   def initialize(word)
-    @my_word = word
+    @my_word = word.downcase
   end
 
   def match(arr)
-    @anagram = @my_word.downcase
     results = []
     @chars = @my_word.chars.sort
     arr.each do |the_word|
-      results << the_word if the_word.chars.sort == @chars && the_word != @my_word
+      something = the_word.downcase
+      results << the_word if something.chars.sort == @chars && something != @my_word
     end
 
     return results
   end
 end
 
-#not sure where to go
-class Anagram
-  def initialize(word)
-    @my_word = Anagram.new(word)
-  end
+# not sure where to go
+# class Anagram
+#   def initialize(word)
+#     @my_word = Anagram.new(word)
+#   end
 
-  def match(arr)
-    arr.select {|word| @my_word.anagram_of?(word) }
-  end
+#   def match(arr)
+#     arr.select {|word| @my_word.anagram_of?(word) }
+#   end
 
-end
